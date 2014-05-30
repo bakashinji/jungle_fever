dd9d8e6f3e1a13be126fbfac1c6ee7c566454f81
Markus Reiter-Haas <iseratho@gmail.com>
2014-05-30 15:38:23 +0200

    Population/Disease Simulator added; currently limited to 1 Disease only

---
+++ RollABall/Assets/Scripts/Disease.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Disease : MonoBehaviour {
+	
+	public Texture icon;
+	public string diseaseName;
+	public List<AssemblyCSharp.Collectible> fastTreatment;
+	public List<AssemblyCSharp.Collectible> longTimeTreatment;
+	public int baseSpreadingRate = 1; // independent from amount of sick people
+	public int infectionSpreadingRate = 1; // dependent from amout of sick people
+	public int infectionSpreadingPerson = 5; // amount of person need for spreading
+	public float spreadingChance; // chance that spreading is successful
+	//public int incubationTime; // after that time, the disease can be lethal
+	//public double baseDeathRate; // chance a person dies after incubation time
+	//public double incDeathRate; // amount at which the death rate increases
+	public float deathRate = 0.003f;
+	public Population population;
+
+	private float nextActionTime = 0.0f;
+	public float period = 1.0f;
+
+	private int amountTotalKilled = 0;
+	private int amountTotalInfected = 0;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("DiseaseSimulator", 0.1f, 1.0f);
+	}
+
+	void OnGUI() {
+		GUI.DrawTexture(new Rect(10, 40, 30, 30),icon);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*if (Time.time > nextActionTime ) {
+			nextActionTime += period;
+			DiseaseSimulator();
+		}*/
+	}
+
+	void DiseaseSimulator()
+	{
+		int newDead = 0;
+		int newInfected = 0;
+
+		// Sim Infection Start
+
+		Random.seed = Time.time.GetHashCode();
+
+		for (int i = 0; i < baseSpreadingRate; i++) {
+			float rand = Random.Range(0.0f, 1.0f);
+			if (rand < spreadingChance)
+				newInfected++;
+		}
+
+		for (int i = 0; i < population.Infected; i+=infectionSpreadingPerson) {
+			for (int j = 0; j < infectionSpreadingRate; j++){
+				float rand = Random.Range(0.0f, 1.0f);
+				if (rand < spreadingChance)
+					newInfected++;
+			}
+		}
+
+		// Sim Infection End
+
+		// Sim Death Start
+
+		for (int i = 0; i < population.Infected; i++) {
+			float rand = Random.Range(0.0f, 1.0f);
+			if (rand < deathRate)
+				newDead++;
+		}
+
+		// Sim Death End
+
+		amountTotalInfected += newInfected;
+		population.InfectPeople (newInfected);
+		amountTotalKilled += newDead;
+		population.KillPeople (newDead);
+	}
+}
---
+++ RollABall/Assets/Scripts/Population.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Population : MonoBehaviour {
+
+	public GUISkin skin;
+	public double population = 1000000;
+	
+	public double GetPopulation {
+		get {
+			return population;
+		}
+	}
+
+	private int infected = 0;
+
+	public int Infected {
+		get {
+			return infected;
+		}
+	}
+
+
+	private float nextActionTime = 0.0f;
+	public float period = 1.0f;
+
+	public double birthrate = 0.00008; // Per seconds
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("IncPopulation", 0.1f, 1.0f);
+	}
+
+	void OnGUI() {
+		GUI.skin = skin;
+		int intpop = (int)population;
+		string text = intpop.ToString ();
+		GUI.Label(new Rect(10, 10, 100, 30), text);
+		GUI.Label(new Rect(10, 80, 100, 30), infected.ToString());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*if (Time.time > nextActionTime ) {
+			nextActionTime += period;
+			//IncPopulation();
+		}*/
+	}
+
+	void IncPopulation()
+	{
+		double newpeople = population * birthrate;
+		population += newpeople;
+	}
+
+	public void InfectPeople(int newInfected)
+	{
+		infected += newInfected;
+	}
+
+	public void KillPeople(int deadPeople)
+	{
+		population -= deadPeople;
+		infected -= deadPeople;
+	}
+}
