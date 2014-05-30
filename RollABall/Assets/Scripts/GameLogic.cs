756d69484d900cb84f0def8deb2e486888dc16db
Markus Reiter-Haas <iseratho@gmail.com>
2014-05-30 17:37:31 +0200

    game logic skeleton added

---
+++ RollABall/Assets/Scripts/GameLogic.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameLogic : MonoBehaviour {
+
+	Population population;
+	Character character;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CheckLosingCond ();
+	}
+
+	void CheckLosingCond(){
+		if (population.GetPopulation == 0 || character.Hp == 0) {
+			// TODO: output you noob
+			gameReset ();
+		}
+	}
+
+	void gameReset() {
+	}
+}
